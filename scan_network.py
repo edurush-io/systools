03116f5025405238126a5e37b4a591214194cbd4
edurush-io <83179368+edurush-io@users.noreply.github.com>
2021-04-25 13:24:11 -0400

    Update scan_network.py

--- scan_network.py
+++ scan_network.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 """ script for quick scanning of an IP range """
 
@@ -12,7 +12,7 @@ import concurrent.futures
 import random
 
 # variables
-# where we would keep IPs 
+# where we would keep IPs
 ip_list = []
 
 # cidr block
@@ -20,11 +20,11 @@ cidr = ''
 
 # how many threads we are going to use
 n_threads = 100
-timeout = 1 # seconds 
+timeout = 1 # seconds
 # the below values will take place when "--fast" argument is given
 fast_threads = 500
 fast_timeout = 0.5
-# the below values will take place when "--ufast" argument is given ### be careful with these :) 
+# the below values will take place when "--ufast" argument is given ### be careful with these :)
 ufast_threads = 1000
 ufast_timeout = 0.3
 
@@ -33,9 +33,9 @@ total_up = 0
 total_down = 0
 total_unknown = 0
 
-# end variables 
+# end variables
 
-### functions 
+### functions
 def icmp_checksum(data):
     x = sum(x << 8 if i % 2 else x for i, x in enumerate(data)) & 0xFFFFFFFF
     x = (x >> 16) + (x & 0xFFFF)
@@ -44,10 +44,10 @@ def icmp_checksum(data):
 
 def icmp_ping(addr, timeout=1, count=1, data=b''):
     global total_up, total_down, total_unknown
-# from https://gist.github.com/pyos/10980172 
+# from https://gist.github.com/pyos/10980172
     with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as conn:
         payload = struct.pack('!HH', random.randrange(0, 65536), count) + data
-        try: 
+        try:
             conn.connect((addr, 1))
         except:
             print ("Please check network address " + addr)
@@ -83,8 +83,8 @@ def cidr_to_list (s):
 def parse_args ():
     global cidr, n_threads, timeout, max_n_threads, min_timeout
     """ Argument parser"""
-    parser = argparse.ArgumentParser ( 
-        description = "Quick network CIDR scanner", 
+    parser = argparse.ArgumentParser (
+        description = "Quick network CIDR scanner",
         usage = "%(prog)s CIDR (e.g. abc.def.gh.yz/nn)"
     )
     parser.add_argument ("cidr", help="CIDR block")
@@ -104,12 +104,11 @@ def parse_args ():
 if __name__ == "__main__":
     parse_args()
     cidr_to_list(cidr)
-    
-# use concurrent executor to speed up the execution 
+
+# use concurrent executor to speed up the execution
     with concurrent.futures.ThreadPoolExecutor ( max_workers = n_threads ) as executor:
         task = {executor.submit(icmp_ping, I, timeout):I for I in ip_list}
         for future in concurrent.futures.as_completed(task):
             future.result()
 
     print ("Total up: {}\nTotal down: {}\nTotal unknown: {}".format(str(total_up), str(total_down), str(total_unknown)))
-    
